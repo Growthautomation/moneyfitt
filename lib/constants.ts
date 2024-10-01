0998b473541e5edcff009d56c289e7e2fd0e9869
yeaung276 <yeyintaung.ya276@gmail.com>
2024-10-01 11:56:32 +0700

    feat: formalise scope, gender, religion, and language

--- lib/constants.ts
+++ lib/constants.ts
@@ -32,7 +32,39 @@ export const broadScope = [
   },
 ];
 
-export const narrowScope = [];
+export const narrowScope = [
+  { code: "BIRM", name: "Business Insurance and Risk Management" },
+  { code: "CEI", name: "Children (Education Savings, Child Health Insurance)" },
+  { code: "CII", name: "Critical Illness/Income Protection" },
+  { code: "CTP", name: "Corporate Tax Planning" },
+  { code: "CHS", name: "CPF Health Schemes" },
+  { code: "CIS", name: "CPF Investment Schemes" },
+  { code: "DMSL", name: "Debt Management and Student Loans" },
+  { code: "DFP", name: "Divorce Financial Planning" },
+  { code: "ECLS", name: "Elder Care and Long-Term Support" },
+  { code: "EFP", name: "Expat Financial Planning" },
+  { code: "FCP", name: "Family and Child Planning" },
+  { code: "GZ", name: "Gen Z" },
+  { code: "HMC", name: "Health and Medical Coverage" },
+  { code: "HNWP", name: "High Net Worth Planning" },
+  { code: "HCH", name: "Housing and CPF for Homes" },
+  { code: "LEP", name: "Legacy and Estate Planning" },
+  { code: "LI", name: "Life Insurance" },
+  { code: "MA", name: "Middle Aged (40-60)" },
+  { code: "M", name: "Millennials" },
+  { code: "P", name: "Pets" },
+  { code: "PRP", name: "Pre-Retirement Planning" },
+  { code: "PAP", name: "Property and Asset Protection" },
+  { code: "RCP", name: "Retirement and CPF Planning" },
+  { code: "SO", name: "Singaporeans Overseas" },
+  { code: "SCP", name: "Special Circumstances Planning" },
+  { code: "SNP", name: "Special Needs Planning" },
+  { code: "SEP", name: "Succession and Exit Planning" },
+  { code: "SII", name: "Sustainable and Impact Investing" },
+  { code: "TP", name: "Tax Planning" },
+  { code: "TI", name: "Travel Insurance" },
+  { code: "WCI", name: "Wealth Creation and Investments" },
+];
 
 export const ageGroups = [
   {
@@ -70,4 +102,30 @@ export const Gender = [
     code: "O",
     name: "Other",
   },
+];
+
+export const Languages = [
+  {code: "ENG", name: "English"},
+  {code: "CHI", name: "Mandarin"},
+  {code: "MAL", name: "Malay"},
+  {code: "TAM", name: "Tamil"},
+  {code: "HOK", name: "Hokkien"},
+  {code: "CAN", name: "Cantonese"},
+  {code: "TEO", name: "Teochew"},
+  {code: "HAK", name: "Hakka"},
+  {code: "BEN", name: "Bengali"},
+  {code: "PUN", name: "Punjabi"},
+];
+
+export const Religion = [
+  {code: "CHR", name: "Christianity"},
+  {code: "ISL", name: "Islam"},
+  {code: "BUD", name: "Buddhism"},
+  {code: "HIN", name: "Hinduism"},
+  {code: "JAI", name: "Jainism"},
+  {code: "SIK", name: "Sikhism"},
+  {code: "JUD", name: "Judaism"},
+  {code: "BAH", name: "Bahá'í"},
+  {code: "ZOR", name: "Zoroastrianism"},
+  {code: "TEN", name: "Tengrism"},
 ];
