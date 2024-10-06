a2f6bea0fc133ae38beec5b2a9b7fea5a4f796f2
MikeBorman1 <mike.borman@yahoo.com>
2024-10-06 13:56:05 +0700

    Logo added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import Header from '@/components/header'
 import '@/styles/globals.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 // TODO: add suspense for every page and high latency component so only that component show loading while the rest is visible
 
@@ -16,6 +18,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <div className="absolute top-4 left-4 z-50">
+          <Link href="/">
+            <Image 
+              src="/Full_colored@2x.webp" 
+              alt="Logo" 
+              width={200} 
+              height={80} 
+              quality={100}
+            />
+          </Link>
+        </div>
         <Header />
         {children}
       </body>
