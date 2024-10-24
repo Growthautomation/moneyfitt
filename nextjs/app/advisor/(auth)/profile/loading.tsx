f8ba58374dfd47d28b083ca65cf9fd2b97e7c47e
yeaung276 <yeyintaung.ya276@gmail.com>
2024-10-24 11:22:11 +0700

    feat: add sentry

---
+++ nextjs/app/advisor/(auth)/profile/loading.tsx
@@ -0,0 +1,9 @@
+import PageLoading from "@/components/utils/page-loading";
+
+export default function Loading() {
+  return (
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center">
+      <PageLoading />
+    </div>
+  );
+}
