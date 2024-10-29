19c6caa4fd2860a69521c705a80cf184361a6fb1
yeaung276 <yeyintaung.ya276@gmail.com>
2024-10-29 15:09:01 +0700

    feat: adjust rematch delay

--- nextjs/components/client/advisor-list/rematch.tsx
+++ nextjs/components/client/advisor-list/rematch.tsx
@@ -1,13 +1,12 @@
-import { createClient } from "@/lib/supabase/server"
 import { Matching } from "@/types/matching"
 import RematchBtn from "./rematch-btn";
-import { addHours, addMinutes, isAfter } from "date-fns";
+import { addHours, isAfter } from "date-fns";
 
 interface RematchProps {
   matching: Matching
 }
 export default function Rematch({matching}: RematchProps) {
-  if(isAfter(new Date(), addMinutes(matching.created_at, 1))){
+  if(isAfter(new Date(), addHours(matching.created_at, 24))){
     return <RematchBtn matchId={matching.id} />
   }
 
