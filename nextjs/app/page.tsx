720ca67c8025458c1ef957b0a896bee4848d7d50
MikeBorman1 <mike.borman@yahoo.com>
2024-10-30 13:50:33 +0700

    changes

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   } = await supabase.auth.getUser();
 
   if (!user) {
-    return redirect("/sign-in");
+    return redirect("https://www.moneyfitt.co/");
   }
 
   return redirect("/home")
