511f75d9089be1b236fda8429c0492b81f9048d5
yeaung276 <yeyintaung.ya276@gmail.com>
2024-11-07 10:46:10 +0700

    feat: fix

--- supabase/functions/unread-messages/search.ts
+++ supabase/functions/unread-messages/search.ts
@@ -1,4 +1,4 @@
-import { SupabaseClient } from '@supabase/supabase-js';
+import { SupabaseClient } from "jsr:@supabase/supabase-js@2";
 import { Database } from "../database.types.ts";
 
 // deno-lint-ignore no-explicit-any
